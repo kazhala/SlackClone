eb9ecb28efbf9397f41b5fc9cd5db81c18c775a6
kevin zhuang <43941510+kazhala@users.noreply.github.com>
2019-09-19 15:01:18 +1000

    add listner for messages

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,27 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import firebase from '../../firebase';
-import { Segment, Comment, Message } from 'semantic-ui-react';
+import { Segment, Comment } from 'semantic-ui-react';
 import MessagesHeader from './MessagesHeader/MessagesHeader';
 import MessagesForm from './MessagesForm/MessagesForm';
+import Message from './Message/Message';
+import { useListVals } from 'react-firebase-hooks/database';
 
 const Messages = props => {
+    const { currentChannel, currentUser } = props;
+
     const messagesRef = firebase.database().ref('messages');
+
+
+
+    const [snapshots, loading, error] = useListVals(currentChannel ? messagesRef.child(currentChannel.id) : null);
+
+
+
+    useEffect(() => {
+        return () => {
+            if (!loading) {
+                messagesRef.child(messagesRef.child(currentChannel.id));
+            }
+        }
+        //eslint-disable-next-line
+    }, []);
 
 
     return (
         <React.Fragment>
             <MessagesHeader />
-
             <Segment>
                 <Comment.Group className="messages">
-
+                    {snapshots && snapshots.map(message => (
+                        <Message
+                            key={message.timestamp}
+                            message={message}
+                            user={currentUser}
+                        />
+                    ))}
                 </Comment.Group>
             </Segment>
-
             <MessagesForm
                 messagesRef={messagesRef}
-                currentUser={props.currentUser}
-                currentChannel={props.currentChannel} />
+                currentUser={currentUser}
+                currentChannel={currentChannel} />
         </React.Fragment>
     );
 }
