d8f6c5cf3723083be7feee7bb083277285803542
kevin zhuang <43941510+kazhala@users.noreply.github.com>
2019-09-24 20:54:29 +1000

    added scroll for sidebar, hide scroll bar

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -13,7 +13,8 @@ const SidePanel = props => {
             inverted
             fixed="left"
             vertical
-            style={{ background: "#4c3c4c", fontSize: '1.2rem' }}
+            style={{ background: "#4c3c4c", fontSize: '1.2rem', overflowY: 'auto', }}
+            className="sideScroll"
         >
             <UserPanel user={props.user} />
             <Starred />
